f7c8e0b9cf3548de77f3f0cfc126e6cd52ed56a2
alereimer <aalexandru.roman@gmail.com>
2025-01-06 00:51:16 +0200

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,26 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Comoara fermecata")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "\n🍕Welcome to Python Pizza Deliveries!🍕\n"
 )
+print("\n🍕Welcome to Python Pizza Deliveries!🍕\n")
+size = input("What size pizza do you want? S, M or L: ")
+if size == "M":
+    pizza1 = 20
+if size == "S":
+    pizza1 = 15
+if size == "L":
+    pizza1 = 25
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+if pepperoni  == "Y":
+    if size == size == "M":
+        pizza1 += 2
+    elif size == "S":
+        pizza1 += 2
+    elif size == "L":
+        pizza1 += 3
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+if extra_cheese == "Y":
+    pizza1 += 1
+print(f"Your final bill is: ${pizza1}.")
